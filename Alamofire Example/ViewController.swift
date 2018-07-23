272c6e7ffc698dc8e7cae761af57b2074d230a96
velkonost <velkonost@gmail.com>
2018-07-23 12:44:43 +0600

    finish

--- Alamofire Example/ViewController.swift
+++ Alamofire Example/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Alamofire
 class ViewController: UIViewController {
+	fileprivate var items = [Item]()
+	@IBOutlet weak var tableView: UITableView!
+	
 	@IBAction func sendRequest(_ sender: UIButton) {
 		Alamofire.request("https://jsonplaceholder.typicode.com/photos").responseJSON {
 			response in
@@ -16,17 +19,23 @@ class ViewController: UIViewController {
 				print("Ошибка при запросе данных \(String(describing: response.result.error))")
 				return
 			}
-			print(response.value)
+			
+			guard let arrayOfItems = response.result.value as? [[String: AnyObject]] else {
+				return
+			}
+			
+			for itm in arrayOfItems {
+				let item = Item(albumId: itm["albumId"] as! Int, id: itm["id"] as! Int, title: itm["title"] as! String, url: itm["url"] as! String)
+				self.items.append(item)
+			}
+			
+			DispatchQueue.main.async {
+				self.tableView.reloadData()
+			}
 		}
 	}
-/*Alamofire.request("https://jsonplaceholder.typicode.com/photos").responseJSON { response in
-guard response.result.isSuccess else {
-print("Ошибка при запросе данных \(String(describing: response.result.error))")
-return
-}
-print(response.value)
-}
-*/
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -39,4 +48,47 @@ print(response.value)
 
 
 }
+
+class ItemCell: UITableViewCell {
+	@IBOutlet weak var idLabel: UILabel!
+	@IBOutlet weak var urlLabel: UILabel!
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var albumIdLabel: UILabel!
+	
+}
+
+struct Item {
+	let albumId: Int
+	let id: Int
+	let title: String
+	let url: String
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return items.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! ItemCell
+		configureCell(cell: cell, for: indexPath)
+		
+		return cell
+	}
+	
+	private func configureCell(cell: ItemCell, for indexPath: IndexPath) {
+		let item = items[indexPath.row]
+		cell.idLabel.text = "\(item.id)"
+		cell.albumIdLabel.text = "\(item.albumId)"
+		cell.urlLabel.text = "\(item.url)"
+		cell.titleLabel.text = "\(item.title)"
+	}
+}
+
+
+
+
+
+
+
 
